8deb639ae34cbdda00f69728c92426bf0ee5bc28
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2024-07-26 19:27:47 +0700

    Mom animations and try door script

--- Assets/FromStore/Free Wood Door Pack/Script/Door.cs
+++ Assets/FromStore/Free Wood Door Pack/Script/Door.cs
@@ -4,40 +4,73 @@ using UnityEngine;
 namespace DoorScript
 {
 	[RequireComponent(typeof(AudioSource))]
-
-
-public class Door : MonoBehaviour {
-	public bool open;
-	public float smooth = 1.0f;
-	float DoorOpenAngle = -90.0f;
-    float DoorCloseAngle = 0.0f;
-	public AudioSource asource;
-	public AudioClip openDoor,closeDoor;
-	// Use this for initialization
-	void Start () {
-		asource = GetComponent<AudioSource> ();
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		if (open)
+	
+	public class Door : MonoBehaviour 
+	{
+	
+		public bool open;
+		public bool isCanOpen;
+		private bool isTryOpen;
+		private int framesTry = 5;
+		private int frame;
+		public float smooth = 1.0f;
+		float DoorOpenAngle = -90.0f;
+	    float DoorCloseAngle = 0.0f;
+		float DoorOpenAngleTry = -5f;
+		public AudioSource asource;
+		public AudioClip openDoor,closeDoor, tryOpen;
+		void Start () 
 		{
-            var target = Quaternion.Euler (0, DoorOpenAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
-	
+			asource = GetComponent<AudioSource> ();
 		}
-		else
-		{
-            var target1= Quaternion.Euler (0, DoorCloseAngle, 0);
-            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
-	
-		}  
-	}
+		
+		void Update () {
+			if (open)
+			{
+	            var target = Quaternion.Euler (0, DoorOpenAngle, 0);
+	            transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+		
+			}
+			else
+			{
+	            var target1= Quaternion.Euler (0, DoorCloseAngle, 0);
+	            transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+		
+			}
+			if (!isCanOpen)
+			{
+				if (isTryOpen)
+				{
+					frame++;
+					if (frame > framesTry)
+						isTryOpen = false;
+					var target = Quaternion.Euler(0, DoorOpenAngleTry, 0);
+					transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
 
-	public void OpenDoor(){
-		open =!open;
-		asource.clip = open?openDoor:closeDoor;
-		asource.Play ();
+				}
+				else
+				{
+					var target1 = Quaternion.Euler(0, DoorCloseAngle, 0);
+					transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+				}
+			}
+		}
+	
+		public void OpenDoor()
+		{
+			if (!isCanOpen)
+			{
+				frame = 0;
+				isTryOpen = true;
+				asource.clip = tryOpen;
+				asource.Play();
+				return;
+			}
+
+			open =!open;
+			asource.clip = open?openDoor:closeDoor;
+			asource.Play ();
+		}
 	}
-}
 }
